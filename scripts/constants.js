221527f5275c7aaa5a493e872ff47d4eac4c1bd4
Aaron Zhong <aaronzhongnz@gmail.com>
2026-01-27 15:56:23 +1300

    lowercase photoid

--- scripts/constants.js
+++ scripts/constants.js
@@ -12,7 +12,7 @@ export const ALL_TRUST_LISTS = ['all_trust_lists'];
  * Supported document types for common identification documents
  */
 export const DocumentType = {
-    PHOTO_ID: 'org.iso.23220.photoID.1',
+    PHOTO_ID: 'org.iso.23220.photoid.1',
     EU_PERSONAL_ID: 'eu.europa.ec.eudi.pid.1',
     JAPAN_MY_NUMBER_CARD: 'org.iso.23220.1.jp.mnc',
     MOBILE_DRIVERS_LICENSE: 'org.iso.18013.5.1.mDL',
@@ -47,7 +47,7 @@ export const createCredentialId = (format, documentType) => {
 };
 
 export const CredentialId = {
-    'cred-mso_mdoc-org_iso_23220_photoID_1': { format: CredentialFormat.MSO_MDOC, documentType: DocumentType.PHOTO_ID },
+    'cred-mso_mdoc-org_iso_23220_photoid_1': { format: CredentialFormat.MSO_MDOC, documentType: DocumentType.PHOTO_ID },
     'cred-mso_mdoc-eu_europa_ec_eudi_pid_1': { format: CredentialFormat.MSO_MDOC, documentType: DocumentType.EU_PERSONAL_ID },
     'cred-mso_mdoc-org_iso_23220_1_jp_mnc': { format: CredentialFormat.MSO_MDOC, documentType: DocumentType.JAPAN_MY_NUMBER_CARD },
     'cred-mso_mdoc-org_iso_18013_5_1_mDL': { format: CredentialFormat.MSO_MDOC, documentType: DocumentType.MOBILE_DRIVERS_LICENSE },
